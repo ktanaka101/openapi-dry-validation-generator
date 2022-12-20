4cf47ab1ff76166a5965a8dd564d2fd55b0bb3bb
ktanaka101 <kentanaka101@gmail.com>
2022-12-20 22:36:04 +0900

    wip

--- tests/query.rs
+++ tests/query.rs
@@ -531,7 +531,7 @@ mod tests {
                 [
                     {
                         "in": "query",
-                        "name": "user_id",
+                        "name": "nested_integer",
                         "required": true,
                         "schema": {
                             "type": "array",
@@ -546,9 +546,82 @@ mod tests {
                                     "maxItems": 6,
                                     "minItems": 5,
                                     "items": {
+                                        "type": "integer",
+                                        "maximum": 8,
+                                        "minimum": 7
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    {
+                        "in": "query",
+                        "name": "nested_string",
+                        "required": true,
+                        "schema": {
+                            "type": "array",
+                            "maxItems": 10,
+                            "minItems": 9,
+                            "items": {
+                                "type": "array",
+                                "maxItems": 12,
+                                "minItems": 11,
+                                "items": {
+                                    "type": "array",
+                                    "maxItems": 14,
+                                    "minItems": 13,
+                                    "items": {
                                         "type": "string",
-                                        "maxLength": 8,
-                                        "minLength": 7
+                                        "maxLength": 16,
+                                        "minLength": 15
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    {
+                        "in": "query",
+                        "name": "nested_boolean",
+                        "required": true,
+                        "schema": {
+                            "type": "array",
+                            "maxItems": 18,
+                            "minItems": 17,
+                            "items": {
+                                "type": "array",
+                                "maxItems": 20,
+                                "minItems": 19,
+                                "items": {
+                                    "type": "array",
+                                    "maxItems": 22,
+                                    "minItems": 21,
+                                    "items": {
+                                        "type": "boolean"
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    {
+                        "in": "query",
+                        "name": "nested_array",
+                        "required": true,
+                        "schema": {
+                            "type": "array",
+                            "maxItems": 24,
+                            "minItems": 23,
+                            "items": {
+                                "type": "array",
+                                "maxItems": 26,
+                                "minItems": 25,
+                                "items": {
+                                    "type": "array",
+                                    "maxItems": 28,
+                                    "minItems": 27,
+                                    "items": {
+                                        "type": "array",
+                                        "maxItems": 30,
+                                        "minItems": 29
                                     }
                                 }
                             }
@@ -558,9 +631,24 @@ mod tests {
             "#,
             expect![[r#"
                 TestExample = Dry::Schema::Params do
-                  required(:user_id).value(:array, max_size: 2, min_size: 1).each(:array?, max_size: 4, min_size: 3) do
+                  required(:nested_integer).value(:array, max_size: 2, min_size: 1).each(:array?, max_size: 4, min_size: 3) do
                     schema(:array?).each(:array?, max_size: 6, min_size: 5) do
-                      schema(:array?).each(:str?, max_size: 8, min_size: 7)
+                      schema(:array?).each(:int?, max: 8, min: 7)
+                    end
+                  end
+                  required(:nested_string).value(:array, max_size: 10, min_size: 9).each(:array?, max_size: 12, min_size: 11) do
+                    schema(:array?).each(:array?, max_size: 14, min_size: 13) do
+                      schema(:array?).each(:str?, max_size: 16, min_size: 15)
+                    end
+                  end
+                  required(:nested_boolean).value(:array, max_size: 18, min_size: 17).each(:array?, max_size: 20, min_size: 19) do
+                    schema(:array?).each(:array?, max_size: 22, min_size: 21) do
+                      schema(:array?).each(:bool?)
+                    end
+                  end
+                  required(:nested_array).value(:array, max_size: 24, min_size: 23).each(:array?, max_size: 26, min_size: 25) do
+                    schema(:array?).each(:array?, max_size: 28, min_size: 27) do
+                      schema(:array?).each(:array?, max_size: 30, min_size: 29)
                     end
                   end
                 end
