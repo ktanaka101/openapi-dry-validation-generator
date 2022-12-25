93f18ec6aea647456b2e7adf6da6717fbda4458c
ktanaka101 <kentanaka101@gmail.com>
2022-12-25 23:21:50 +0900

    wip

--- src/ast_builder/reference_db.rs
+++ src/ast_builder/reference_db.rs
@@ -1,11 +1,9 @@
-use std::{
-    collections::HashMap,
-    path::{Path, PathBuf},
-};
+use std::{collections::HashMap, path::PathBuf};
 
 use anyhow::Result;
 
 use openapiv3::PathItem;
+use url::Url;
 
 use crate::{select_file_type, SupportFileType};
 
@@ -25,8 +23,8 @@ impl ReferenceDatabase {
 
         let entry = self.path_item_by_file.entry(reference);
         Ok(entry.or_insert_with_key(|reference| {
-            let file_type = select_file_type(&reference).unwrap();
-            let file = std::fs::read_to_string(reference).unwrap();
+            let file_type = reference.file_type().unwrap();
+            let file = reference.read_content();
 
             let item: PathItem = match file_type {
                 SupportFileType::Json => serde_json::from_str(&file).unwrap(),
@@ -38,17 +36,42 @@ impl ReferenceDatabase {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-struct FileKey(PathBuf);
-impl FileKey {
-    fn new<P>(path: P) -> Result<Self>
-    where
-        P: AsRef<Path>,
-    {
-        Ok(Self(std::fs::canonicalize(path)?))
-    }
+enum FileKey {
+    Local(PathBuf),
+    Remote(Url),
 }
-impl AsRef<Path> for FileKey {
-    fn as_ref(&self) -> &Path {
-        &self.0
+impl FileKey {
+    fn new(path: &str) -> Result<Self> {
+        if path.starts_with("https://") || path.starts_with("http://") {
+            Ok(Self::Remote(url::Url::parse(path)?))
+        } else {
+            Ok(Self::Local(std::fs::canonicalize(path)?))
+        }
+    }
+
+    fn file_type(&self) -> Result<SupportFileType> {
+        match self {
+            FileKey::Local(path) => select_file_type(path),
+            FileKey::Remote(url) => {
+                let url = url.to_string();
+                if url.ends_with(".json") {
+                    Ok(SupportFileType::Json)
+                } else if url.ends_with(".yaml") || url.ends_with(".yml") {
+                    Ok(SupportFileType::Yaml)
+                } else {
+                    anyhow::bail!(format!("Unknown file type. url: {url}"))
+                }
+            }
+        }
+    }
+
+    fn read_content(&self) -> String {
+        match self {
+            FileKey::Local(path) => std::fs::read_to_string(path).unwrap(),
+            FileKey::Remote(url) => reqwest::blocking::get(url.to_string())
+                .unwrap()
+                .text()
+                .unwrap(),
+        }
     }
 }
