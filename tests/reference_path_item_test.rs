9c0155f500dc9f2549817d976a23abb29c516e42
ktanaka101 <kentanaka101@gmail.com>
2022-12-28 12:57:51 +0900

    wip

--- tests/reference_path_item_test.rs
+++ tests/reference_path_item_test.rs
@@ -3,6 +3,15 @@ use std::fs::remove_file;
 use expect_test::{expect, Expect};
 use openapi_dry_validation_generator::generate_dry_validation_from_root_json;
 
+mod common;
+
+use httptest::{matchers::*, responders::*, Expectation, Server};
+
+fn check(actual: &str, expect: Expect) {
+    let openapi = generate_dry_validation_from_root_json(actual);
+    expect.assert_eq(&openapi);
+}
+
 fn check_with_local_file(actual: &str, expect: Expect) {
     let mut inputs = actual.split("---");
     let openapi = inputs.next().unwrap();
@@ -74,5 +83,92 @@ fn reference_path_item_from_local_file() {
           optional(:ref_string_key).value(:string)
         end
     "#]],
+    );
+}
+
+#[test]
+fn reference_path_item_by_json_from_server() {
+    let server = Server::run();
+    let stub_body = r#"
+        {
+            "get": {
+                "operationId": "testExample",
+                "parameters": [
+                    {
+                        "in": "query",
+                        "name": "ref_string_key",
+                        "schema": {
+                            "type": "string"
+                        }
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "OK"
+                    }
+                }
+            }
+        }
+    "#;
+    server.expect(
+        Expectation::matching(request::method_path("GET", "/foo.json"))
+            .times(1)
+            .respond_with(status_code(200).body(stub_body)),
+    );
+    let url = server.url("/foo.json");
+
+    let openapi = common::boilerplate(&format!(
+        r#"
+            "/example/test": {{
+                "$ref": "{url}"
+            }}
+        "#
+    ));
+    check(
+        &openapi,
+        expect![[r#"
+            TestExample = Dry::Schema::Params do
+              optional(:ref_string_key).value(:string)
+            end
+        "#]],
+    );
+}
+
+#[test]
+fn reference_path_item_by_yaml_from_server() {
+    let server = Server::run();
+    let stub_body = r#"
+        get:
+            operationId: testExample
+            parameters:
+                - in: query
+                  name: ref_string_key
+                  schema:
+                      type: string
+            responses:
+                200:
+                    description: OK
+    "#;
+    server.expect(
+        Expectation::matching(request::method_path("GET", "/foo.yaml"))
+            .times(1)
+            .respond_with(status_code(200).body(stub_body)),
+    );
+    let url = server.url("/foo.yaml");
+
+    let openapi = common::boilerplate(&format!(
+        r#"
+            "/example/test": {{
+                "$ref": "{url}"
+            }}
+        "#
+    ));
+    check(
+        &openapi,
+        expect![[r#"
+            TestExample = Dry::Schema::Params do
+              optional(:ref_string_key).value(:string)
+            end
+        "#]],
     );
 }
